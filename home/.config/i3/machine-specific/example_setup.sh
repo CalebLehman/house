9d9cb51fc3b720afc423e3f88eaf5359bbdd86fe
topol <TopologicalInvariant@gmail.com>
2023-11-10 08:28:42 -0500

    Add machine-specific i3 setup examples

--- home/.config/i3/machine-specific/example_setup.sh
+++ home/.config/i3/machine-specific/example_setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
-if [ -f "${HOME}/.config/i3/machine-specific/background" ]; then
-  feh --bg-scale "${HOME}/.config/i3/machine-specific/background"
-fi
+# Manage display settings
+# xrandr --output eDP-1 --mode 1280x720
+
+# Set background
+# if [ -f "${HOME}/.config/i3/machine-specific/background" ]; then
+#   feh --bg-scale "${HOME}/.config/i3/machine-specific/background"
+# fi
+
+# Select random background from `wallpapers`
+# bg=$(fd -IH -e jpg -e png . "${HOME}/.config/i3/machine-specific/wallpapers" | shuf -n1)
+# feh --bg-scale "${bg}"
