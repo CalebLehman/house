ca6bd4e0d4716d7bb1e7e5738c3223ca76f20b2c
topol <TopologicalInvariant@gmail.com>
2024-02-09 00:02:54 -0500

    Add more decorate themes

--- decorate/themes/__init__.py
+++ decorate/themes/__init__.py
@@ -1,12 +1,22 @@
+from .alabaster_dark import alabaster_dark
+from .base16_default import base16_default
 from .dracula import dracula
+from .everforest_light import everforest_light
+from .material import material
 from .one_dark import one_dark
 from .one_light import one_light
+from .taerminal import taerminal
 from .theme import Theme
 
 themes: [Theme] = [
+    alabaster_dark,
+    base16_default,
     dracula,
+    everforest_light,
+    material,
     one_dark,
     one_light,
+    taerminal,
 ]
 
 def get_theme(name: str) -> Theme:
